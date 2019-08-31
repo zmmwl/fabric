b48244e9c6b329a3f07a54e2f7bc8b4c678377d5
zmmwl <zmmwl@aliyun.com>
2019-09-01 01:46:14 +0800

    zmm

---
+++ zmm/script/mac/env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /Users/zmmwl/dev/blockchain/hyperledger/fabric-1.4/src/github.com/hyperledger/fabric
+
+export GOPATH="/Users/zmmwl/dev/blockchain/hyperledger/fabric-1.4"
+
+export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+
---
+++ zmm/script/work/env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /opt/dev/hyperledger/zmm/fabric/src/github.com/hyperledger/fabric
+
+export GOPATH="/opt/dev/hyperledger/zmm/fabric"
+
+export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+
