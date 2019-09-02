7ae66608be416cad0310fb12b99dc2461d41d858
zhoumengmeng 00419767 <zhoumengmeng3@huawei.com>
2019-09-02 11:13:03 +0800

    zmm

--- zmm/script/work/env.sh
+++ zmm/script/work/env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd /opt/dev/hyperledger/zmm/fabric/src/github.com/hyperledger/fabric
+cd /opt/dev/hyperledger/zmm/fabric-1.4/src/github.com/hyperledger/fabric
 
-export GOPATH="/opt/dev/hyperledger/zmm/fabric"
+export GOPATH="/opt/dev/hyperledger/zmm/fabric-1.4"
 
 export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 
